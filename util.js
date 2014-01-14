5923363e6c80ac047a39d399d9c068d312edd407
John Wu <webmaster@leapoahead.com>
2014-01-15 01:17:28 +0800

    style matter

--- util.js
+++ util.js
@@ -8,10 +8,9 @@ define(function(require, exports, module){
         
         var a = left.split('.'),
             b = right.split('.'),
-            i = 0, 
             len = Math.max(a.length, b.length);
             
-        for (; i < len; i = i + 1) {
+        for (i = 0; i < len; i = i + 1) {
             if ((a[i] && !b[i] && parseInt(a[i]) > 0) || (parseInt(a[i]) > parseInt(b[i]))) {
                 return 1;
             } else if ((b[i] && !a[i] && parseInt(b[i]) > 0) || (parseInt(a[i]) < parseInt(b[i]))) {
